531b5e79edafb945bb879c8c8c43ff804c3a0a37
Aditya88Gupta <prince88aditya@gmail.com>
2021-02-03 13:48:27 +0530

    Reconstructing Solution

--- Placing Parentheses/Max Value of Expression.cpp
+++ Placing Parentheses/Max Value of Expression.cpp
@@ -7,9 +7,11 @@
 using namespace std;
 
 long long **Min(NULL),**Max(NULL);  // Value of expression can be very large
+vector<int> Start,End;             //  Vector storing indeces where backets should be inserted 
 
 long long MaxValue(vector<int> numbers,vector<char> operations);
 void Min_Max(int i,int j,vector<char> operations);
+void Reconstruct(int i,int j,vector<int> numbers,vector<char> operations,bool flag);
 
 int main(){
     string Str;
@@ -29,7 +31,32 @@ int main(){
         Min[i]=new long long[n];   
         Max[i]=new long long[n];
     } 
-    cout<<MaxValue(numbers,operations); 
+    cout<<MaxValue(numbers,operations);cout<<endl;
+    Reconstruct(0,n-1,numbers,operations,true);
+    bool tmp=false;n=0;
+    for(int i=0;i<Str.size();i++){
+        n=0;
+        for(int j=0;j<Start.size();j++){
+            if (i==Start[j]){
+                if (Start[j]!=End[j]){
+                   cout<<"( ";
+                } 
+            }
+            if (i==End[j]){
+                if (Start[j]!=End[j]){
+                   n++;
+                   tmp=true;
+                } 
+            }
+        }
+        cout<<Str[i]<<" ";
+        if(tmp){
+            tmp=false;
+            for(int k=0;k<n;k++)
+               cout<<")";
+            cout<<" ";   
+        }
+    }
 }
 
 long long MaxValue(vector<int> numbers,vector<char> operations){
@@ -91,4 +118,84 @@ void Min_Max(int i,int j,vector<char> operations){
         Max[i][j]=min({sub1,sub2,sub3,sub4,Max[i][j]},Larger);  // Largest Among all Subexpression
     }
 
+}
+
+void Reconstruct(int i,int j,vector<int> numbers,vector<char> operations,bool flag){
+    if (i>=j)
+       return;
+    char opk;
+    for(int k=i;k<j;k++){
+        opk=operations[k];
+        switch (opk){
+        case '-':
+            if (flag==true){
+               if (Max[i][k]-Min[k+1][j]==Max[i][j]){
+                   Start.push_back(i*2);Start.push_back((k+1)*2);
+                   End.push_back(k*2);End.push_back(j*2);
+                   Reconstruct(i,k,numbers,operations,true);
+                   Reconstruct(k+1,j,numbers,operations,false);
+               }
+            }  
+            else{
+                if (Min[i][k]-Max[k+1][j]==Min[i][j]){
+                    Start.push_back(i*2);Start.push_back((k+1)*2);
+                    End.push_back(k*2);End.push_back(j*2);
+                    Reconstruct(i,k,numbers,operations,false);
+                    Reconstruct(k+1,j,numbers,operations,true);
+                }
+            }
+            break;     
+        case '+':
+            if (flag==true){
+               if (Max[i][k]+Max[k+1][j]==Max[i][j]){
+                   Start.push_back(i*2);Start.push_back((k+1)*2);
+                   End.push_back(k*2);End.push_back(j*2);
+                   Reconstruct(i,k,numbers,operations,true);
+                   Reconstruct(k+1,j,numbers,operations,true);
+               }
+            }
+            else{
+                if (Min[i][k]+Min[k+1][j]==Min[i][j]){
+                   Start.push_back(i*2);Start.push_back((k+1)*2);
+                   End.push_back(k*2);End.push_back(j*2);
+                   Reconstruct(i,k,numbers,operations,false);
+                   Reconstruct(k+1,j,numbers,operations,false);
+                }
+            }
+            break;
+        case '*':
+            if (flag==true){
+               if (Max[i][k]*Max[k+1][j]==Max[i][j]){
+                   Start.push_back(i*2);Start.push_back((k+1)*2);
+                   End.push_back(k*2);End.push_back(j*2);
+                   Reconstruct(i,k,numbers,operations,true);
+                   Reconstruct(k+1,j,numbers,operations,true);
+               }
+               else if (Min[i][k]*Min[k+1][j]==Max[i][j]){
+                   Start.push_back(i*2);Start.push_back((k+1)*2);
+                   End.push_back(k*2);End.push_back(j*2);
+                   Reconstruct(i,k,numbers,operations,false);
+                   Reconstruct(k+1,j,numbers,operations,false);
+               }
+            }
+            else{
+               if (Max[i][k]*Min[k+1][j]==Min[i][j]){
+                   Start.push_back(i*2);Start.push_back((k+1)*2);
+                   End.push_back(k*2);End.push_back(j*2);
+                   Reconstruct(i,k,numbers,operations,true);
+                   Reconstruct(k+1,j,numbers,operations,false);
+               }
+               else if (Min[i][k]*Max[k+1][j]==Min[i][j]){
+                   Start.push_back(i*2);Start.push_back((k+1)*2);
+                   End.push_back(k*2);End.push_back(j*2);
+                   Reconstruct(i,k,numbers,operations,false);
+                   Reconstruct(k+1,j,numbers,operations,true);
+               }
+            }
+            break;        
+        default:
+            break;
+        }
+
+    }   
 }
